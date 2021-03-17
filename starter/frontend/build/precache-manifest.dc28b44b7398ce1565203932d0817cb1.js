8551eab16acf6c7f9d398dc508bb647ee50b5b5e
Gage Cates <gagecates10@gmail.com>
2021-03-16 18:59:45 -0600

    add react static build folder

---
+++ starter/frontend/build/precache-manifest.dc28b44b7398ce1565203932d0817cb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7ad62395ddb1295c2bb8262da5c357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a37d7390671faf2cb7",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "072f3e95e3a0453af94c",
+    "url": "/static/js/2.6d5d0db4.chunk.js"
+  },
+  {
+    "revision": "83a37d7390671faf2cb7",
+    "url": "/static/js/main.71b6f84c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
